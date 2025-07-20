3134b2ef8172cbf7ec910d2fe3dd157fd4f130b6
Giovanni Tinervia <giovannitinervia9@gmail.com>
2025-07-20 19:01:03 +0200

    modified documentation of formulae functions

--- R/formulae.R
+++ R/formulae.R
@@ -27,6 +27,62 @@ classify_term <- function(x) {
 }
 
 
+#' Build linear components
+#'
+#' @param formula A formula specifying a linear predictor.
+#' @param data A data.frame containign the variables in the formula.
+#'
+#' @returns A list containing the `model.matrix` `X` and the zero Penalty
+#' matrix `P`.
+#'
+#' @export
+#' @importFrom stats model.matrix as.formula terms
+build_linear <- function(formula, data) {
+  formula <- as.formula(formula)
+  X <- model.matrix(formula, data)
+  P <- matrix(0, ncol(X), ncol(X))
+  list(X = X, P = P)
+}
+
+
+#' Build smooth components
+#'
+#' @param smooth_term A smooth term specified by [mgcv::s()].
+#' @param data A data.frame containign the variables to build the smooth components.
+#'
+#' @returns A list containing the `model.matrix` `X` and the penalty matrix `P`.
+#'
+#' @export
+#' @importFrom mgcv s
+#' @importFrom Matrix bdiag
+build_smooth <- function(smooth_term, data) {
+  if (is.character(smooth_term)) {
+    smooth_term <- eval(parse(text = smooth_term))
+  }
+  r <- mgcv::smoothCon(
+    smooth_term,
+    data = data,
+    absorb.cons = TRUE,
+    scale.penalty = FALSE
+  )
+  nbasis <- length(r)
+  Xlist <- vector("list", nbasis)
+  Plist <- vector("list", nbasis)
+  for (i in 1:nbasis) {
+    Xj <- r[[i]]$X
+    Pj <- r[[i]]$S[[1]]
+    colnames(Xj) <- paste0(r[[i]]$label, "_", 1:ncol(Xj))
+    Xlist[[i]] <- Xj
+    Plist[[i]] <- Pj
+  }
+  X <- do.call(cbind, Xlist)
+  P <- as.matrix(Matrix::bdiag(Plist))
+  id <- rep(1:nbasis, sapply(Xlist, ncol))
+  colnames(P) <- rownames(P) <- colnames(X)
+  list(X = X, P = P, id = id)
+}
+
+
 #' Interpret a formula
 #'
 #' @param formula An object of class `formula`.
@@ -71,7 +127,8 @@ interpret_formula <- function(formula) {
 #' @param distrib An object of class [distrib].
 #' @param data A data.frame containign the variables in the formula.
 #'
-#' @returns
+#' @returns A list for each parameter of the distribution containing model matrices and penalty matrices
+#' for each term of the formula
 #'
 #' @export
 interpret_formulae <- function(formulae, distrib, data) {
@@ -85,75 +142,34 @@ interpret_formulae <- function(formulae, distrib, data) {
   }
 
   names(f_split) <- distrib$parameters
+
+  f_split <- lapply(f_split, function(f) {
+    if (!grepl("~", f)) {
+      paste0("~ ", f)
+    } else {
+      f
+    }
+  })
+
   f_list <- lapply(f_split, as.formula)
 
   response <- deparse(f_list[[1]][[2]])
 
   f_int <- lapply(f_list, interpret_formula)
 
+  f_comp <- vector("list", nform)
+  names(f_comp) <- distrib$parameters
+
   for (i in 1:nform) {
     parts <- f_int[[i]]$parts
     types <- f_int[[i]]$types
     funs <- paste0("build_", types)
-    Map(
+    f_comp[[i]] <- Map(
       function(funs, parts) match.fun(funs)(parts, data),
       funs = funs,
       parts = parts
     )
   }
-}
 
-
-#' Build linear components
-#'
-#' @param formula A formula specifying a linear predictor.
-#' @param data A data.frame containign the variables in the formula.
-#'
-#' @returns A list containing the `model.matrix` `X` and the zero Penalty
-#' matrix `P`.
-#'
-#' @export
-#' @importFrom stats model.matrix as.formula terms
-build_linear <- function(formula, data) {
-  formula <- as.formula(formula)
-  X <- model.matrix(formula, data)
-  P <- matrix(0, ncol(X), ncol(X))
-  list(X = X, P = P)
-}
-
-#' Build smooth components
-#'
-#' @param smooth_term A smooth term specified by [mgcv::s()].
-#' @param data A data.frame containign the variables to build the smooth components.
-#'
-#' @returns A list containing the `model.matrix` `X` and the penalty matrix `P`.
-#'
-#' @export
-#' @importFrom mgcv s
-#' @importFrom Matrix bdiag
-build_smooth <- function(smooth_term, data) {
-  if (is.character(smooth_term)) {
-    smooth_term <- eval(parse(text = smooth_term))
-  }
-  r <- mgcv::smoothCon(
-    smooth_term,
-    data = data,
-    absorb.cons = TRUE,
-    scale.penalty = FALSE
-  )
-  nbasis <- length(r)
-  Xlist <- vector("list", nbasis)
-  Plist <- vector("list", nbasis)
-  for (i in 1:nbasis) {
-    Xj <- r[[i]]$X
-    Pj <- r[[i]]$S[[1]]
-    colnames(Xj) <- paste0(r[[i]]$label, "_", 1:ncol(Xj))
-    Xlist[[i]] <- Xj
-    Plist[[i]] <- Pj
-  }
-  X <- do.call(cbind, X)
-  P <- as.matrix(Matrix::bdiag(P))
-  id <- rep(1:nbasis, sapply(Xlist, ncol))
-  colnames(P) <- rownames(P) <- colnames(X)
-  list(X = X, P = P, id = id)
+  f_comp
 }
